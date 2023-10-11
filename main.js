4c21d0365a805f1f8175448c62e060e485e8d707
Orchiwi <allan.morlet13@gmail.com>
2023-10-11 17:30:58 +0200

    function TrameValide

--- main.js
+++ main.js
@@ -4,6 +4,12 @@ function Ecrire()
     client.open("POST","client_udp.php", false);
     donneesJson='{"univers":"0","adresse":"172.20.21.183","valeur":"'+valeur+'"}';
     client.send(donneesJson);
-
-
+}
+function Mon_popup()
+{
+    alert("Gestion de l'evenement 'mouse over' sur mon bouton");
+}
+function TrameValide()
+{
+    console.debug("vérification de la trame");
 }
---
+++ test.js
@@ -0,0 +1,9 @@
+var message = "La somme fait : ";
+var val1 = 15;
+var val2 = 4;
+var resultat = val1 + val2;
+console.log("VAL1 vaut : " + val1);
+console.log("VAL2 vaut : " + val2);
+console.log("RESULTAT vaut : " + resultat);
+console.log(message + resultat);
+console.log(message + val1 + val2);
